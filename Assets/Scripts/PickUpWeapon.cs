cb5a1d179c9adfed4a2a9b1158fe8e3c538d50c7
SamxHub <alexandra.haller16@gmail.com>
2022-11-21 17:28:33 +0100

    PickUpWeapon code

---
+++ Assets/Scripts/PickUpWeapon.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpWeapon : MonoBehaviour
+{
+    [SerializeField] private Collider _pickUprange;
+    [SerializeField] private GameObject _WeaponPlace;
+    [SerializeField] private float _Thrust = 20f;
+    private bool _hasWeapon = false;
+    private GameObject _currentWeapon;
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log(other.transform.tag);
+        if (other.transform.tag == "Weapon" && Input.GetMouseButtonUp(1))
+        {
+            if (_hasWeapon == true)
+            {
+                Transform childToRemove = this.transform.Find(_currentWeapon.name);
+                childToRemove.parent = null;
+                //_currentWeapon.GetComponent<Rigidbody>().AddForce(_currentWeapon.transform.forward * _Thrust);
+
+                _hasWeapon = true;
+
+                _currentWeapon = other.gameObject;
+                Debug.Log("true");
+                WeaponPos(other);
+            }
+            else
+            {
+                _hasWeapon = true;
+
+                _currentWeapon = other.gameObject;
+                Debug.Log("true");
+                WeaponPos(other);
+            }
+        }
+    }
+
+    private void WeaponPos(Collider other)
+    {
+        other.transform.position = _WeaponPlace.transform.position;
+        other.transform.rotation = _WeaponPlace.transform.rotation;
+        other.transform.parent = this.transform;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
