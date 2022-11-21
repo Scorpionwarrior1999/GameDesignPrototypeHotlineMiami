a8feda7688bbac4e3d6f6ce1403e215f54abf988
SamxHub <alexandra.haller16@gmail.com>
2022-11-21 17:25:11 +0100

    Base weapon
    Added pickup of weapon and able to put it down.
    No force yet (causes bug),

--- Assets/Scripts/WeaponPickup.cs
+++ Assets/Scripts/WeaponPickup.cs
@@ -1,25 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponPickup : MonoBehaviour
 {
+    [SerializeField] private Collider _pickUprange;
+    [SerializeField] private GameObject _WeaponPlace;
+    [SerializeField] private float _Thrust = 20f;
+    private bool _hasWeapon = false;
+    private GameObject _currentWeapon;
+    
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log(other.transform.tag);
+        if (other.transform.tag == "Weapon" && Input.GetMouseButtonUp(1))
+        {
+            if (_hasWeapon == true)
+            {
+                Transform childToRemove = this.transform.Find(_currentWeapon.name);
+                childToRemove.parent = null;
+                //_currentWeapon.GetComponent<Rigidbody>().AddForce(_currentWeapon.transform.forward * _Thrust);
 
+                _hasWeapon = true;
 
+                _currentWeapon = other.gameObject;
+                Debug.Log("true");
+                WeaponPos(other);
+            }
+            else
+            {
+                _hasWeapon = true;
 
-    // als enemy in collideer van weapon is en hasweapon van enemy is false, zet hasweapon van enemy op true en destroy weapon
+                _currentWeapon = other.gameObject;
+                Debug.Log("true");
+                WeaponPos(other);
+            }
+        }
+    }
 
-
+    private void WeaponPos(Collider other)
+    {
+        other.transform.position = _WeaponPlace.transform.position;
+        other.transform.rotation = _WeaponPlace.transform.rotation;
+        other.transform.parent = this.transform;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
